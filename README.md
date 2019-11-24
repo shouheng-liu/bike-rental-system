dfa79a3e5906f852c739a01cbaaf53c01ddac133
Klausstaler <fredrik.klauss@gmx.de>
2019-11-24 20:47:04 +0000

    Modify Bike implementation, update README

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # bike-rental-system
+EVERY TASK HAS TO IMPLEMENT TESTS AS WELL!
+## Fred's tasks
+Get interface implementation working
+Currently working on Bike, BikeType and interfaces
+##Shouheng's task
+
+Curently working on Location, DateRange
--- src/uk/ac/ed/bikerental/Bike.java
+++ src/uk/ac/ed/bikerental/Bike.java
@@ -1,15 +1,24 @@
 package uk.ac.ed.bikerental;
 
 
+import java.math.BigDecimal;
 import java.time.LocalDate;
 
 public class Bike {
+    private static int identifierCount;
     private BikeState bikeState;
     private LocalDate manufacturingDate;
     private BikeTypes bikeType;
+    private int condition;
+    private int identifier;
 
-    public Bike(BikeTypes bikeType) {
-
+    public Bike(BikeTypes bikeType, int condition, Location returnLocation,
+                BigDecimal price, BigDecimal replacementValue,
+                BigDecimal deposit) {
+        identifierCount++;
+        this.identifier = identifierCount;
+        this.condition = condition;
+        this.BikeState = BikeState.from("inShop");
         this.bikeType = bikeType;
         this.manufacturingDate = LocalDate.now();
     }
