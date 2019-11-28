545cc333431ba592f0433bbc40e4ec3435250814
shouheng <shouheng@234.90.74.188.in-addr.arpa>
2019-11-28 11:27:58 +0000

    Testing Commit on Eclipse

--- tests/uk/ac/ed/bikerental/ValuationPolicyTests.java
+++ tests/uk/ac/ed/bikerental/ValuationPolicyTests.java
@@ -1,4 +1,11 @@
 package uk.ac.ed.bikerental;
 
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class ValuationPolicyTests {
 }
